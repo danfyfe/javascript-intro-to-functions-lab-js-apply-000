e526705ec11f34c17216648f1131d9beb26d6920
Dan Fyfe <fyfedaniel@gmail.com>
2019-02-13 01:37:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ function sayHiToGrandma(string){
   } else if (string === string.toLowerCase()){
     return "I can't hear you!"
   } else if (string === string.toUpperCase()){
-    
+    return "YES INDEED!"
   }
-  
 }
